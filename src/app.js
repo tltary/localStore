708b1c0a06a6fbccf9a95ecababa656d2a7c9e11
Антон Пятков <antonpyatkov@MacBook-Air-Anton.local>
2019-12-10 15:07:14 +0400

    Add crypto cipher, refactoring code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 //import {getGroup, addGroup, editGroup, removeGroup, getItem, addItem, editItem, removeItem} from "./store.js"
 
 import * as store from './store.js';
+
+store.initStore();
+
+console.log(store.getStore())
+
+store.addGroup('test');
+store.addItem('test','222','1');
+
--- src/store.js
+++ src/store.js
@@ -1,175 +1,173 @@
+const CryptoJS = require("crypto-js");
+import { secretKey } from './tuple.js';
+
 const storage = localStorage;
-let env = false;
+let _debug = true;
 
-function _console(text, param) {
-    if (!env) {
-        if (param === 'log') {
-            console.log(text) 
-        } else if (param === 'error') {
-            console.error(text)
-        } 
+const _console = (text, param) => {
+    if (!_debug) {
+        return;
     }
+    console[param](text);
 }
 
-function checkText(str) {
-    if (typeof str === 'undefined') {
-        return false
-    } else {
-        return true;
-    }
+const isValidText = (str) => {
+    return typeof str === 'undefined' ? true : false;
 }
 
-function checkObj(group, name) {
-    let obj = JSON.parse(storage.getItem('storage'))
+const isValidObj = (group, name) => {
+    let obj = decode(storage.getItem('storage'))
     if (typeof name === 'undefined') {
-        if (typeof obj[group] === "undefined") {
-            return true
-        } else {
-            return false;
-        }    
+        return typeof obj[group] === "undefined" ? true : false;
     } else {
-        if (typeof obj[group][name] !== "undefined") {
-            return true
-        } else {
-            return false;
-        }    
+        return typeof obj[group][name] !== "undefined" ? true : false;
     }
+}
+
+const encode = (data) => {
+    return _debug ? JSON.stringify(data) : CryptoJS.AES.encrypt(JSON.stringify(data), secretKey);
+}
+
+const decode = (data) => {
+    let bytes = CryptoJS.AES.decrypt(data, secretKey);
+    return _debug ? JSON.parse(data) : JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 }
 
 export const initStore = (prod) => {
-    if (storage.length === 0) {
-        storage.setItem('storage', '{}')
-        _console(`store init`, 'log')
-    } else {
-        if (storage.getItem('storage')[0] === '{' && storage.getItem('storage')[parseInt(storage.getItem('storage').length) - 1] === '}') {
-            _console(`store alredy init`, 'log')
-        } else {
-            storage.setItem('storage', '{}')
-            _console(`store reinit`, 'log')
-        }
-    }
     if (prod) {
-        env = true;
+        _debug = false;
+    }
+    if (storage.length === 0) {
+        _debug ? storage.setItem('storage', encode('{}')) : storage.setItem('storage', encode(JSON.parse('{}')));
+        _console(`store init`, 'log');
+    } else {
+        let cacheData = decode(storage.getItem('storage'));
+        if (typeof cacheData === 'object') {
+            _console(`store alredy init`, 'log');
+        } else {
+            _debug ? storage.setItem('storage', encode('{}')) : storage.setItem('storage', encode(JSON.parse('{}')));
+            _console(`store reinit`, 'log');
+        }
     }
 }
 
 export const getStore = () => {
-    return JSON.parse(storage.getItem('storage'))
+    return decode(storage.getItem('storage'));
 }
 
 export const getGroup = (nameGroup) => {
-    if (checkText(nameGroup)) {
-        let obj = JSON.parse(storage.getItem('storage'))
-        if (checkObj(nameGroup)) {
-            _console(`Group ${nameGroup} not find, please add group`, 'error');
-            return false;
-        }
-        return obj[nameGroup];
-    } else {
+    let obj = decode(storage.getItem('storage'));
+    if (isValidText(nameGroup)) {
         _console('Group name must be filled', 'error');
+        return;
     }
+    if (isValidObj(nameGroup)) {
+        _console(`Group ${nameGroup} not find, please add group`, 'error');
+        return;
+    }
+    return obj[nameGroup];
 }
 
 export const addGroup = (nameGroup) => {
-    if (checkText(nameGroup)) {
-        let obj = JSON.parse(storage.getItem('storage'))
-        if (!checkObj(nameGroup)) {
-            _console('This group alredy add', 'error')
-            return false;
-        }
-        obj[nameGroup] = {}
-        storage.setItem('storage', JSON.stringify(obj))
-        _console(`Group ${nameGroup} added`, 'log')
-    } else {
-        _console('Group name must be filled', 'error')
+    let obj = decode(storage.getItem('storage'));
+    if (isValidText(nameGroup)) {
+        _console('Group name must be filled', 'error');
+        return;
     }
+    if (!isValidObj(nameGroup)) {
+        _console('This group alredy add', 'error');
+        return;
+    }
+    obj[nameGroup] = {}
+    storage.setItem('storage', encode(obj));
+    _console(`Group ${nameGroup} added`, 'log');
 }
 
 export const removeGroup = (nameGroup) => {
-    if (checkText(nameGroup)) {
-        let obj = JSON.parse(storage.getItem('storage'));
-        if (checkObj(nameGroup)) {
-            _console(`Group ${nameGroup} not find, please add group`, 'error');
-            return false;
-        }
-        delete obj[nameGroup];
-        storage.setItem('storage', JSON.stringify(obj));
-        _console(`Group ${nameGroup} delete`, 'log');
-    } else {
+    let obj = decode(storage.getItem('storage'));
+    if (isValidText(nameGroup)) {
         _console('Group name must be filled', 'error');
+        return;
     }
+    if (isValidObj(nameGroup)) {
+        _console(`Group ${nameGroup} not find, please add group`, 'error');
+        return;
+    }
+    delete obj[nameGroup];
+    storage.setItem('storage', encode(obj));
+    _console(`Group ${nameGroup} delete`, 'log');
 }
 
 export const getItem = (nameGroup, nameItem) => {
-    if (checkText(nameGroup) || checkText(nameItem)) {
-        let obj = JSON.parse(storage.getItem('storage'))
-        if (checkObj(nameGroup)) {
-            _console(`Group ${nameGroup} not find, please add group`, 'error');
-            return false;
-        }
-        if (checkObj(nameGroup, nameItem)) {
-            _console(`Item ${nameItem} not find in this ${nameGroup} group, please add group`, 'error');
-            return false;
-        }
-        return obj[nameGroup][nameItem];
-    } else {
+    let obj = decode(storage.getItem('storage'));
+    if (isValidText(nameGroup) && isValidText(nameItem)) {
         _console(`Group name and item name must be filled`, 'error');
+        return;
     }
+    if (isValidObj(nameGroup)) {
+        _console(`Group ${nameGroup} not find, please add group`, 'error');
+        return;
+    }
+    if (isValidObj(nameGroup, nameItem)) {
+        _console(`Item ${nameItem} not find in this ${nameGroup} group, please add group`, 'error');
+        return;
+    }
+    return obj[nameGroup][nameItem];
 }
 
 export const addItem = (nameGroup, nameItem, textItem) => {
-    if (checkText(nameGroup) || checkText(nameItem) || checkText(textItem)) {
-        let obj = JSON.parse(storage.getItem('storage'))
-        if (checkObj(nameGroup)) {
-            _console(`Group ${nameGroup} not find, please add group`, 'error');
-            return false;
-        }
-        if (checkObj(nameGroup, nameItem)) {
-            _console(`Item ${nameItem} allredy add, please edit this item or delete, if you need`, 'error');
-            return false;
-        }
-        obj[nameGroup][nameItem] = textItem;
-        storage.setItem('storage', JSON.stringify(obj));
-        _console(`Item ${nameItem} - ${textItem} add to ${nameGroup} group`, 'log');
-    } else {
+    let obj = decode(storage.getItem('storage'))
+    
+    if (isValidText(nameGroup) && isValidText(nameItem) && isValidText(textItem)) {
         _console(`Group name and item name and item text must be filled`, 'error');
+        return;
     }
+    if (isValidObj(nameGroup)) {
+        _console(`Group ${nameGroup} not find, please add group`, 'error');
+        return;
+    }
+    if (isValidObj(nameGroup, nameItem)) {
+        _console(`Item ${nameItem} allredy add, please edit this item or delete, if you need`, 'error');
+        return;
+    }
+    obj[nameGroup][nameItem] = textItem;
+    storage.setItem('storage', encode(obj));
+    _console(`Item ${nameItem} - ${textItem} add to ${nameGroup} group`, 'log');
 }
 
 export const editItem = (nameGroup, nameItem, newValue) => {
-    if (checkText(nameGroup) || checkText(nameItem) || checkText(newValue)) {
-        let obj = JSON.parse(storage.getItem('storage'));
-        if (checkObj(nameGroup)) {
-            _console(`Group ${nameGroup} not find, please add group`, 'error');
-            return false;
-        }
-        if (checkObj(nameGroup, nameItem)) {
-            _console(`Item ${nameItem} not found`, 'error');
-            return false;
-        }
-        obj[nameGroup][nameItem] = newValue;
-        storage.setItem('storage', JSON.stringify(obj));
-        _console(`Item ${nameItem} delete`, 'log');
-    } else {
-        _console('Group name and item name and new item value must be filled', 'error')
+    let obj = decode(storage.getItem('storage'));
+    if (isValidText(nameGroup) && isValidText(nameItem) && isValidText(newValue)) {
+        _console('Group name and item name and new item value must be filled', 'error');
+        return;
     }
+    if (isValidObj(nameGroup)) {
+        _console(`Group ${nameGroup} not find, please add group`, 'error');
+        return;
+    }
+    if (isValidObj(nameGroup, nameItem)) {
+        _console(`Item ${nameItem} not found`, 'error');
+        return;
+    }
+    obj[nameGroup][nameItem] = newValue;
+    storage.setItem('storage', encode(obj));
+    _console(`Item ${nameItem} delete`, 'log');
 }
 
 export const removeItem = (nameGroup, nameItem) => {
-    if (checkText(nameGroup) || checkText(nameItem)) {
-        let obj = JSON.parse(storage.getItem('storage'));
-        if (checkObj(nameGroup)) {
-            _console(`Group ${nameGroup} not find, please add group`, 'error');
-            return false;
-        }
-        if (checkObj(nameGroup, nameItem)) {
-            _console(`Item ${nameItem} not found`, 'error');
-            return false;
-        }
-        delete obj[nameGroup][nameItem];
-        storage.setItem('storage', JSON.stringify(obj));
-        _console(`Item ${nameItem} delete`, 'log');
-    } else {
+    let obj = decode(storage.getItem('storage'));
+    if (isValidText(nameGroup) && isValidText(nameItem)) {
         _console('Group name and item name must be filled', 'error');
     }
+    if (isValidObj(nameGroup)) {
+        _console(`Group ${nameGroup} not find, please add group`, 'error');
+        return;
+    }
+    if (isValidObj(nameGroup, nameItem)) {
+        _console(`Item ${nameItem} not found`, 'error');
+        return;
+    }
+    delete obj[nameGroup][nameItem];
+    storage.setItem('storage', encode(obj));
+    _console(`Item ${nameItem} delete`, 'log');
 }
